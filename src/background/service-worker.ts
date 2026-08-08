5cddbb8300827f9659eb962ac94ca37b1118fb80
Tran Duy Quoc <tduyquoc@gmail.com>
2026-08-08 19:28:59 +0700

    Register S35 PDF UX router

--- src/background/service-worker.ts
+++ src/background/service-worker.ts
@@ -1,9 +1,11 @@
 import { registerMessageRouter } from "./message-router";
 import { registerPdfEditorRouter } from "./pdf-editor-router";
 import { registerPdfSourceRouter } from "./pdf-source-router";
+import { registerPdfUxRouter } from "./pdf-ux-router";
 import { registerPersistentJobRouter } from "./persistent-job-router";
 
 registerMessageRouter();
 registerPersistentJobRouter();
 registerPdfEditorRouter();
 registerPdfSourceRouter();
+registerPdfUxRouter();
